c4ce06a8078ac11b69b7fa584756ed2fb1df96b6
岳丰全 <yuefengquan@jiaxinmore.com>
2015-12-01 18:13:32 +0800

    推荐页

--- js/jxm/tpl/recommend.tpl.js
+++ js/jxm/tpl/recommend.tpl.js
@@ -1,7 +1,32 @@
 define(function (require, exports, module) {
     var tpl = 
         '<article class="recommend">\
-            <p>测试文字</p>\
+            <div class="recommend_total">\
+				<div class="recommend_total_l">\
+					<span class="title">累计投资用户</span>\
+					<p class="number">20000/人次</p>\
+				</div>\
+				<div class="recommend_total_r">\
+					<span class="title">累计投资金额</span>\
+					<p class="number">3689万元</p>\
+				</div>\
+            </div>\
+            <div class="recommend_detail">
+				<div class="recommend_detail_t">
+					<h2 class="head">天添利</h2>
+				</div>
+				<div class="recommend_detail_m">
+					<div class="chart">
+
+					</div>
+					<div class="info">
+
+					</div>
+				</div>
+				<div class="recommend_detail_b">
+					<input type="button" value="查看" class="godetail" />
+				</div>
+            </div>
         </article>';
     module.exports = tpl;
 })
--- js/jxm/views/recommend.js
+++ js/jxm/views/recommend.js
@@ -1,45 +1,17 @@
-//我的银行卡
+//推荐页面
 define(function (require, exports, module) {
-    var recommendTpl = require('jxm/tpl/recommend.tpl');
-    var hammer = require('hammer');
-    var Model = require("jxm/model/model");
-    var footer = require('jxm/tpl/card.footer.tpl');
-    var getAddrModel = new Model.getAddrModel();
-    var tool = require('jxm/utils/Tool');
-    var handle = new tool();
-    var message = '网络错误，请稍后重试';
-    var self;
-    var timeTemp=0;
-    module.exports = App.Page.extend({
-        onShow: function () {
-            self.setHeader()
-            self.$el.html(recommendTpl);
-        },
-       
-        setHeader: function () {
-            var header = new App.UI.UIHeader();
-            header.set({
-                view: this,
-                title: '加薪猫月度对账单',
-                back: {
-                    'tagname': 'invite', 'value': '',
-                    itemFn: function () {
-                        return '<span class="right_txt_btn user js_invite" id="abcd">邀请好友</span><span id="abc">absssc</span>';
-                    },
-                    callback: function () {
-                             App.goBack()
-                    }
-                },
-                right: null
-            });
-        },
-        afterMount: function () {
-            self=this
-        },
-        onHide: function(){
-            this.$el.html('');
-        }
-    })
+    var recommend = require('jxm/tpl/recommend.tpl');
+    var footer = require('jxm/tpl/footer.tpl');
+    var self='';
+        module.exports = App.Page.extend({
+            initialize:function(){
+                self = this;
+            },
+            afterMount: function(){
+                self.$el.html(recommend + footer);
+            },
+            
+        })
 })
 
 
