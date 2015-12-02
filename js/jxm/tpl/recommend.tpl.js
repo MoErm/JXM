2c09d37bbdc05507865cd664b844bc0006f0f150
岳丰全 <yuefengquan@jiaxinmore.com>
2015-12-02 18:15:10 +0800

    推荐页面

--- js/jxm/tpl/recommend.tpl.js
+++ js/jxm/tpl/recommend.tpl.js
@@ -1,6 +1,12 @@
 define(function (require, exports, module) {
     var tpl = 
-        '<article class="recommend">\
+    	'<article class="recommend">\
+    		<div class="recommend_ad">\
+				<div class="mod_focus">\
+			        <div class="img_box">\
+			        </div>\
+			    </div>\
+    		</div>\
             <div class="recommend_total">\
 				<div class="recommend_total_l">\
 					<span class="title">累计投资用户</span>\
@@ -11,22 +17,32 @@ define(function (require, exports, module) {
 					<p class="number">3689万元</p>\
 				</div>\
             </div>\
-            <div class="recommend_detail">
-				<div class="recommend_detail_t">
-					<h2 class="head">天添利</h2>
-				</div>
-				<div class="recommend_detail_m">
-					<div class="chart">
-
-					</div>
-					<div class="info">
-
-					</div>
-				</div>
-				<div class="recommend_detail_b">
-					<input type="button" value="查看" class="godetail" />
-				</div>
-            </div>
+            <div class="recommend_detail">\
+				<div class="recommend_detail_t">\
+					<h2 class="head"><em class="title">天添利</em>收益率会涨的活期理财</h2>\
+				</div>\
+				<div class="recommend_detail_m">\
+					<div class="chart">\
+						<svg id="chart_line" width="300" height="100">\
+						    <path d="M50 300 C41 10,50 14,300 1" stroke="#000" fill="none" style="stroke-width: 3px;"></path>\
+						</svg>\
+						<p class="chart_min"><em>5%</em>起天天加息</p> \
+						<p class="chart_max"><em>12%</em>最高年化收益率</p>\
+					</div>\
+					<div class="info">\
+						<ul class="info_list">\
+							<li><em class="list_one"></em><span>收益率递增</span></li>\
+							<li><em class="list_two"></em><span>当日起息</span></li>\
+							<li><em class="list_three"></em><span>24小时可赎</span></li>\
+							<li><em class="list_four"></em><span>100%保障</span></li>\
+						</ul>\
+					</div>\
+				</div>\
+				<div class="recommend_detail_b">\
+					<a href="#" class="godetail">查看详情</a>\
+				</div>\
+            </div>\
         </article>';
+
     module.exports = tpl;
 })
--- js/jxm/views/recommend.js
+++ js/jxm/views/recommend.js
@@ -1,25 +1,71 @@
 //推荐页面
-define(function (require, exports, module) {
+define(function(require, exports, module) {
     var recommend = require('jxm/tpl/recommend.tpl');
     var footer = require('jxm/tpl/footer.tpl');
-    var self='';
-        module.exports = App.Page.extend({
-            initialize:function(){
-                self = this;
-            },
-            afterMount: function(){
-                self.$el.html(recommend + footer);
-            },
-            
-        })
+    var snapSvg= require("jxm/utils/snap.svg-min");
+    var imageSlider = null;
+    var self = null;
+    module.exports = App.Page.extend({
+        initialize: function() {
+            return this;
+        },
+        onShow: function() {
+            self = this.initialize();
+            //隐藏header
+            var header = document.querySelector("#header");
+            $(header).hide();
+            //添加内容
+            self.$el.html(recommend + footer);
+            //轮播滚动
+            self.initAD();
+            self.initChart();
+        },
+        initAD: function() {
+            var container = self.$el.find(".img_box");
+            var minHeight = $(window).width() / 3.2;
+            var imgs = [{
+                id: 4,
+                src: './images/zbs11.jpg',
+                href: 'http://mp.weixin.qq.com/s?__biz=MzA5NDk4NDA5Ng==&mid=400413790&idx=1&sn=ef4e9f3d4ac84b9d09f6d3671833cd1a#wechat_redirect'
+            }, {
+                id: 2,
+                src: './images/xszn.jpg',
+                href: 'http://mp.weixin.qq.com/s?__biz=MzA5NDk4NDA5Ng==&mid=209894037&idx=1&sn=5ad856a2d275475c801c6a0604874843#rd'
+            }, {
+                id: 3,
+                src: './images/xszy.jpg',
+                href: 'http://mp.weixin.qq.com/s?__biz=MzA5NDk4NDA5Ng==&mid=210062014&idx=1&sn=babbf5cda487369cf0ae489719e12a73#rd '
+            }];
+
+            container.css({
+                "max-height": minHeight,
+                "min-height": minHeight,
+                "height": minHeight
+            });
+
+            if (!imageSlider) {
+                imageSlider = new App.UI.UIImageSlider({
+                    datamodel: {
+                        data: imgs,
+                        itemFn: function(item) {
+                            return item.src ? '<img width=100% src="' + item.src + '" data-href="' + item.href + '">' : '</img>';
+                        }
+                    },
+                    displayNum: 1,
+                    wrapper: container,
+                    autoPlay: true,
+                    itemClick: function(e) {
+                        window.location.href = e.href;
+                    }
+                });
+            } else {
+                imageSlider.datamodel.data = imgs;
+                imageSlider.refresh();
+            }
+            imageSlider.show();
+        },
+        initChart: function(){
+            var chartLine= Snap("#chart_line");
+        }
+    })
 })
-
-
-
-
-
-
-
-
-
-
