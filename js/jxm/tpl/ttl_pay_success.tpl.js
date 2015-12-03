177fef60c09862d54d038510f6a21c279feae765
岳丰全 <yuefengquan@jiaxinmore.com>
2015-12-03 18:19:03 +0800

    更新成功页面

--- js/jxm/tpl/ttl_pay_success.tpl.js
+++ js/jxm/tpl/ttl_pay_success.tpl.js
@@ -1,22 +1,28 @@
 define(function (require, exports, module) {
     var tpl ='<article class="ttl_pay_success">\
-		<div class="ttl_pay_success_content">\
+		<div class="ttl_pay_success_t">\
 			<ul class="success_list">\
-				<li class="info">\
-					<em class="sub_icon"></em>\
-					<p class="sub_total">成功支付  30,000 元</p>\
-					<p>交通银行（尾号6535）</p>\
-					<p>2015/11/15  11:20:33</p>\
+				<li class="card">\
+					<em class="icon"></em>\
+					<div class="info">\
+						<p class="sub_total">成功支付  30,000 元</p>\
+						<p>交通银行（尾号6535）</p>\
+						<p>2015/11/15  11:20:33</p>\
+					</div>\
 				</li>\
 				<li class="time">\
-					<em class="sub_icon"></em>\
-					<p class="sub_time">2015/11/15</p>\
-					<p>开始计算收益，今日收益率5.000%</p>\
+					<em class="icon"></em>\
+					<div class="info">\
+						<p class="sub_time">2015/11/15</p>\
+						<p>开始计算收益，今日收益率5.000%</p>\
+					</div>\
 				</li>\
 				<li class="show">\
-					<em class="sub_icon"></em>\
-					<p class="sub_show">2015/11/15</p>\
-					<p>显示收益</p>\
+					<em class="icon"></em>\
+					<div class="info">\
+						<p class="sub_show">2015/11/16</p>\
+						<p>显示收益</p>\
+					</div>\
 				</li>\
 			</ul>\
 		</div>\
