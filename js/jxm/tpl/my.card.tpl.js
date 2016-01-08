f86c589d306edee9a4fb6dc55c214d68585c89cb
倪文斌 <niwenbin@jiaxinmore.com>
2016-01-08 14:54:37 +0800

    修改

--- js/jxm/tpl/my.card.tpl.js
+++ js/jxm/tpl/my.card.tpl.js
@@ -3,7 +3,7 @@ define(function (require, exports, module) {
                  <% _.each(cardList, function(card){%>\
                  <div class="add_card">\
                      <div class="stripe"></div>\
-                     <div class="card_info"><span class="card_logo"><img src="<%=cardList.bankLogo%>" width="36" alt=""></span><span class="card_name"><%=card.bankName%></span><span class="card_type">借记卡</span></div>\
+                     <div class="card_info"><span class="card_logo"><img src="<%=card.bankLogo%>" width="36" alt=""></span><span class="card_name"><%=card.bankName%></span><span class="card_type">借记卡</span></div>\
                         <p class="card_numb"><%=card.cardNo%></p>\
                         <p class="card_other">支付单笔<span class="numb"><%=card.transactLimit%></span>单日<span class="numb"><%=card.dailyLimit%></span></p>\
                    </div>\
