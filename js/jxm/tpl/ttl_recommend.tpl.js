422d868afdc359a384aa594bd4254064d1687f50
倪文斌 <niwenbin@jiaxinmore.com>
2016-03-03 17:19:08 +0800

    横幅

--- js/jxm/tpl/ttl_recommend.tpl.js
+++ js/jxm/tpl/ttl_recommend.tpl.js
@@ -1,6 +1,7 @@
 define(function (require, exports, module) {
-    var tpl = 
-    	'\
+    var tpl =
+        '<div class="notice "><div class="notice_tran">亲爱的用户，加薪猫平台即将全面升级和改版，目前的新手活动和邀请活动将于2016年3月10日22:00结束，特此通知。</div></div>\
+<div style="clear: both"></div>\
     <article class="ttl_recommend" id="ttl_recommend">\
     		<div class="ttl_recommend_ad">\
 				<div class="mod_focus">\
